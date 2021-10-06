e662ba9b2f6f12e916062cb503f44dcc0b012587
SpecialSmiler <358317446@qq.com>
2021-10-06 14:05:16 +0800

    Add instruction of FpsConverter in README

--- FpsConverter/Program.cs
+++ FpsConverter/Program.cs
@@ -7,27 +7,32 @@ namespace SSmiler.FpsConverter
 		static void Main(string[] args)
 		{
 
-			for (int i = 1; i <= 10; i++)
-			{
-				for (int j = i; j <= 10; j++)
-				{
-					Console.Write($"({j},{i}): ");
-					PrintArray(FpsConverter.GetStepsArrayFps(j, i));
-				}
-			}
-
-			//PrintArray(fpsConverter.GetStepsArrayFps(6, 3));
-			//PrintArray(fpsConverter.GetStepsArrayFps(10, 9));
-			//PrintArray(fpsConverter.GetStepsArrayFps(7, 4));
-			//PrintArray(fpsConverter.GetStepsArrayFps(3, 2));
+			//for (int i = 1; i <= 10; i++)
+			//{
+			//	for (int j = i; j <= 10; j++)
+			//	{
+			//		//Console.Write($"(InputFps:{j,3}, OutputFps:{i,3})");
+			//		PrintArray(FpsConverter.GetStepsArrayFps(j, i));
+			//	}
+			//}
+			PrintArray(FpsConverter.GetStepsArrayFps(10, 7), 10, 7);
+			PrintArray(FpsConverter.GetStepsArrayFps(11, 7), 11, 7);
+			PrintArray(FpsConverter.GetStepsArrayFps(12, 7), 12, 7);
 		}
 
-		static void PrintArray(int[] arr)
+		static void PrintArray(int[] arr, int input = -1, int output = -1)
 		{
+			if(input >= 0 && output >= 0)
+			{
+				Console.Write($"(InputFps:{input,3}, OutputFps:{output,3})");
+			}
+
+			Console.Write("[ ");
 			foreach (var a in arr)
 			{
 				Console.Write(a + " ");
 			}
+			Console.Write("]");
 			Console.WriteLine();
 		}
 	}
